f415cf50d85af392aef79dd009c473e05800cfa3
hector141093 <c21.hector@gmail.com>
2016-05-23 01:28:40 -0600

    Altas Bajas tarea

---
+++ AltasBajas/js/eventos.js
@@ -0,0 +1,205 @@
+var iniciaApp = function()
+{
+	var banderaAlta=true;
+	var validarEntrada = function()
+	{			
+		//Invalida los eventos que 
+		//no corresponden a esta función.	
+		event.preventDefault();
+		var usuario = $("#txtUsuario").val();
+		var clave   = $("#txtClave").val();
+		//******** Validaciones **********
+		//1.- Que no sean vacíos
+		if(usuario == "")
+		{
+			alert("El usuario no debe ser vacío");
+			$("#txtUsuario").focus();
+		}
+		if(clave == "")
+		{
+			alert("La clave no debe ser vacía");
+			$("#txtClave").focus();
+		}
+		//2.- Verificar usuario y contraseña
+		var parametros="accion=validaEntrada"+
+					   "&usuario="+usuario+
+					   "&clave="+clave+
+					   "&id="+Math.random(); 
+		$.ajax({
+			beforeSend:function(){
+				console.log("Validar al usuario");
+			},
+			cache: false,
+			type: "POST",
+			dataType: "json",
+			url:"php/funciones.php",
+			data:parametros,
+			success: function(response){
+				if(response.respuesta == true) //¬¬
+				{
+					$("#datosUsuario").hide();
+					$("#alertNoEntra").hide();
+					$("nav").show("slow");
+					$("#alertEntra").html("<strong> ¡Bienvenido "+usuario+"!</strong>");
+					$("#alertEntra").show("slow");
+				}
+				else
+				{					
+					$("#alertNoEntra").show("fast");
+				}
+			},
+			error: function(xhr,ajaxOptions,thrownError){
+				console.log("Algo salió mal");
+			}
+		});
+		console.log("Se disparó el submit");
+	}
+
+	var Altas = function()
+	{
+		banderaAlta=true;
+		//Ocultamos los alertas en caso que exista alguno
+		$(".alert").hide("fast");
+		$(".elemBaja").show("fast");
+		
+		//Mostramos el formulario
+		$("#altaUsuarios").show("slow");
+		$("#altaUsuarios h2").html("Alta Usuarios");
+		//enciendo la funcion de alta usuario
+		$("#frmAltaUsuarios").on("submit",AltaUsuario);
+		//Apago la función de BajaUsuariao para el 
+		//mismo botón
+		$("#frmAltaUsuarios").off("submit",BajaUsuario);
+	}
+
+	var AltaUsuario = function()
+	{
+		event.preventDefault();
+		//alert($("#frmAltaUsuarios").serialize());
+		var datos = $("#frmAltaUsuarios").serialize();
+
+		console.log(datos);
+		var parametros = "accion=guardaUsuario&"+datos+
+		                 "&id="+Math.random();
+		$.ajax({
+			beforeSend:function(){
+				console.log("Guardar al usuario");
+			},
+			cache: false,
+			type: "POST",
+			dataType: "json",
+			url:"php/funciones.php",
+			data:parametros,
+			success: function(response){
+				if(response.respuesta == true )//¬¬
+				{
+					$("#alertAltaExito").show("slow");
+				}
+				else
+				{
+					$("#alertAltaSinExito").show("slow");
+				}
+			},
+			error: function(xhr,ajax,thrownError){
+
+			}
+		});
+	}
+	var Bajas= function()
+	{
+		banderaAlta=false;
+		$(".alert").hide("fast");
+		$(".elemBaja").hide("fast");
+
+		$("#altaUsuarios").show("slow");		
+		$("#altaUsuarios h2").html("Baja Usuarios");
+		//apago la funcion de alta usuario
+		$("#frmAltaUsuarios").off("submit",AltaUsuario);
+		//Enciendo la función de BajaUsuariao para el 
+		//mismo botón
+		$("#frmAltaUsuarios").on("submit",BajaUsuario);
+	}
+	var BajaUsuario = function(){
+		event.preventDefault();
+		//alert($("#frmAltaUsuarios").serialize());
+		var datos = $("#txtNombreUsuario").val();
+		console.log(datos);
+		var parametros = "accion=bajaUsuario&"+"usuario="+datos+
+		                 "&id="+Math.random();	 
+
+		$.ajax({
+			beforeSend:function(){
+				console.log("Guardar al usuario");
+			},
+			cache: false,
+			type: "POST",
+			dataType: "json",
+			url:"php/funciones.php",
+			data:parametros,
+			success: function(response){
+				if(response.respuesta == true )//¬¬
+				{
+					$("#alertBajaSinExito").hide("fast");
+					$("#alertBajaExito").show("slow");
+				}
+				else
+				{	
+					$("#alertBajaExito").hide("fast");
+					$("#alertBajaSinExito").show("slow");
+				}
+			},
+			error: function(xhr,ajax,thrownError){
+
+			}
+		});
+	}
+//AGREGUE
+	var LlenarCampos = function(){
+		event.preventDefault();
+		var datos=$("#frmAltaUsuarios").serialize();
+//		var datos = $("#txtNombreUsuario").val();
+		
+		var parametros = "accion=LlenarCampos&"+datos+
+		                 "&id="+Math.random();		              
+		$.ajax({
+			beforeSend:function(){
+				console.log("Trayendo Datos");
+			},
+			cache: false,
+			type: "POST",
+			dataType: "json",
+			url:"php/funciones.php",
+			data:parametros,
+			success: function(response){
+				if(response.respuesta && !banderaAlta )//¬¬
+				{
+				
+					$("#txtNombreUsuario").val(response.nom);		
+					$("#txtClaveUsuario").val(response.cla);
+					$("#txtTipoUsuario").val(response.tipo);
+					$("#txtDepartamento").val(response.depto);
+					$(".elemBaja").show("slow");
+				}
+				else
+				{	
+				
+				}
+			},
+			error: function(xhr,ajax,thrownError){
+
+			}
+		});
+		 console.log(datos);
+	}
+
+	var Salir= function(){
+		location.reload();		
+	}
+	$("#frmValidaEntrada").on("submit",validarEntrada);
+	$("#btnAltas").on("click",Altas);
+	$("#frmAltaUsuarios").on("submit",AltaUsuario);
+	$("#btnBajas").on("click",Bajas);
+	$("#btnSalir").on("click",Salir);
+	$("#txtNombreUsuario").on( "focusout", LlenarCampos );
+}
+$(document).on("ready",iniciaApp);
---
+++ AltasBajas/php/funciones.php
@@ -0,0 +1,139 @@
+<?php
+//Funciones
+
+function GetSQLValueString($theValue, $theType, $theDefinedValue = "", $theNotDefinedValue = "") 
+{
+  $theValue = get_magic_quotes_gpc() ? stripslashes($theValue) : $theValue;
+
+  $theValue = function_exists("mysql_real_escape_string") ? mysql_real_escape_string($theValue) : mysql_escape_string($theValue);
+
+  switch ($theType) {
+    case "text":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;    
+    case "long":
+    case "int":
+      $theValue = ($theValue != "") ? intval($theValue) : "NULL";
+      break;
+    case "double":
+      $theValue = ($theValue != "") ? "'" . doubleval($theValue) . "'" : "NULL";
+      break;
+    case "date":
+      $theValue = ($theValue != "") ? "'" . $theValue . "'" : "NULL";
+      break;
+    case "defined":
+      $theValue = ($theValue != "") ? $theDefinedValue : $theNotDefinedValue;
+      break;
+  }
+  return $theValue;
+}
+
+function validaEntrada()
+{
+	$usuario = GetSQLValueString($_POST["usuario"],"text");
+	$clave   = GetSQLValueString(md5($_POST["clave"]),"text");
+	$respuesta = false;
+	//Conecto al servidor de BD
+	//Servidor, usuario, clave
+	$conexion = mysql_connect("localhost","root","");
+	//Seleccionar la BD
+	mysql_select_db("cursopw");
+	$validar = sprintf("select usuario,clave from usuarios where usuario=%s and clave=%s limit 1",$usuario,$clave);
+	$resultado = mysql_query($validar);
+	//Preguntamos si se trajo un registro
+	if(mysql_num_rows($resultado) > 0)
+		$respuesta = true;
+	$salidaJSON = array('respuesta' => $respuesta );
+	//Devolvemos el resultado al JS
+	print json_encode($salidaJSON);
+}
+
+function guardaUsuario()
+{
+	$usuario = GetSQLValueString($_POST["txtNombreUsuario"],"text");
+	$clave   = GetSQLValueString(md5($_POST["txtClaveUsuario"]),"text");
+	$tipo    = GetSQLValueString($_POST["txtTipoUsuario"],"text");   
+	$depto   = GetSQLValueString($_POST["txtDepartamento"],"long");
+	$respuesta = false;
+	//Conecto al servidor de BD
+	//Servidor, usuario, clave
+	$conexion = mysql_connect("localhost","root","");
+	//Seleccionar la BD
+	mysql_select_db("cursopw");
+	$guarda = sprintf("insert into usuarios values(%s,%s,%s,%d)",$usuario,$clave,$tipo,$depto);
+	// Ejecutamos la consulta
+	mysql_query($guarda);
+	//Cuantos registros fueron afectados
+	if(mysql_affected_rows() > 0)
+	{
+		$respuesta = true;
+	}
+	$salidaJSON = array('respuesta' => $respuesta);
+	print json_encode($salidaJSON);
+}
+//Agregue
+function LlenarCampos(){
+	$respuesta=false;
+	$usuario = GetSQLValueString($_POST["txtNombreUsuario"],"text");
+	$conexion = mysql_connect("localhost","root","");
+	mysql_select_db("cursopw");
+	mysql_query("SET NAMES utf8"); //caracteres latinos
+	$consulta = sprintf("select * from usuarios where usuario=%s limit 1",$usuario);
+	$resconsulta= mysql_query($consulta);
+	if(mysql_num_rows($resconsulta)>0){ //para saber si hay datos
+		$respuesta= true;
+
+		//rescatar valores
+		$fila=mysql_fetch_array($resconsulta);
+		//$nom= $fila[1];
+		$nom=$fila[0];
+		$cla= $fila[1];
+		$tipo=$fila[2];
+		$depto=$fila[3];
+	}
+
+	$salidaJSON = array ('respuesta'=> $respuesta,'nom'=>$nom,'cla'=>$cla,'tipo'=>$tipo,
+						 'depto'=>$depto);
+	print json_encode ($salidaJSON);
+}
+
+function bajaUsuario(){
+	$usuario=GetSQLValueString($_POST["usuario"],"text");
+	$respuesta=false;
+	$conexion=mysql_connect("localhost","root","");
+	mysql_select_db("cursopw");
+	$baja=sprintf("delete from usuarios where usuario=%s limit 1",$usuario);
+	// $update=sprintf("update usuarios set tipousuario=baja where usuario=%s",$usuario);
+	mysql_query($baja);
+	//el delete update cuantos se afectan
+	if(mysql_affected_rows()>0){
+		$respuesta=true;
+	}
+	$salidaJSON=array('respuesta'=>$respuesta);
+	print json_encode($salidaJSON);
+}
+
+$accion = $_POST["accion"];
+//Menú principal
+switch ($accion) {
+	case 'validaEntrada':
+		validaEntrada();
+		break;
+	case 'guardaUsuario':
+		guardaUsuario();
+		break;
+	case 'LlenarCampos':
+		LlenarCampos();
+		# code...
+		break;
+	case 'bajaUsuario':
+		bajaUsuario();
+			# code...
+		break;
+	default:
+		# code...
+		break;
+		
+//CON EL ENTER MUESTRE DATOS DEL USUARIO
+}
+?>
